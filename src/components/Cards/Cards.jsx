55d1d0f1a7a8377281dfb329e7adc964387e4a48
alhad-tm <alhadchikkutm@gmail.com>
2023-10-08 16:17:53 +0530

    features content changed

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -126,31 +126,37 @@ const Cards = () => {
            {cards===0 &&   <motion.span
              initial={{ opacity: 0, x:1000}}
              whileInView={{ opacity: 1,x:0}}
-              transition={{ duration: 1 }}>Exclusive Event Access: 
-Get VIP entry to thrilling car meets and rides.</motion.span> }
+              transition={{ duration: 1 }}>
+               Exclusive Events: Access thrilling car meets, scenic rides, and exclusive gatherings.
+</motion.span> }
 
 {cards===1 &&  <motion.span
       initial={{ opacity: 0,x:1000}}
       whileInView={{ opacity: 1,x:0}}
-       transition={{ duration: 2 }}> Savings on Services: Enjoy discounts on car maintenance and 
-  accessories. </motion.span>}
+       transition={{ duration: 2 }}> 
+    Savings on Services: Enjoy significant discounts on car maintenance and accessories.
+
+   </motion.span>}
 
   {cards===2 && <motion.span
     initial={{ opacity: 0,x:1000}}
     whileInView={{ opacity: 1,x:0}}
      transition={{ duration: 2 }}>
-    Community Connection: Join a passionate network of car enthusiasts.</motion.span>}
+   Community Connection: Connect with like-minded car enthusiasts and make lifelong friends.
+    </motion.span>}
 
     {cards===3 && <motion.span
       initial={{ opacity: 0,x:1000}}
       whileInView={{ opacity: 1,x:0}}
        transition={{ duration: 2 }}>
-      Lifetime Access to Club Facilities: Batcave sets up dedicated car club facilities, & members get lifetime access.</motion.span>}
+Personalized Perks: Tailored benefits based on your specific interests.
+      </motion.span>}
 
        {cards===4 && <motion.span
-         initial={{ opacity: 0,x:1000}}
+         initial={{ opacity: 1,x:1000}}
          whileInView={{ opacity: 1,x:0}}
-          transition={{ duration: 1.4 }}>  Personalised Benefits: Tailored perks based on your interests.
+          transition={{ duration: 1.4 }}> 
+          Lifetime Access: Enjoy lifetime access to club facilities for camaraderie and car passion.
         </motion.span>}
 
 
