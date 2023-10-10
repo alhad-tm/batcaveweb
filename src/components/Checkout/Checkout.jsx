7630f2dc7e3e504b66ab87371ff014e7f92e5929
alhad-tm <alhadchikkutm@gmail.com>
2023-10-10 11:30:23 +0530

    checkbox changd in checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -115,7 +115,15 @@ const Checkout = () => {
       
     </label>
                   {/* <div className={css.inputline}></div> */}
-               <div className={css.checkboxdiv}> <input className={css.check} type="checkbox"  />
+               <div className={css.checkboxdiv}> 
+               {/* <input className={css.check} type="checkbox"  /> */}
+               <input type="checkbox" id="cbx" className={css.cbx}  /> 
+      <label htmlFor="cbx" className={css.check} >
+        <svg width="18px" height="18px" viewBox="0 0 18 18">
+          <path d="M1,9 L1,3.5 C1,2 2,1 3.5,1 L14.5,1 C16,1 17,2 17,3.5 L17,14.5 C17,16 16,17 14.5,17 L3.5,17 C2,17 1,16 1,14.5 L1,9 Z"></path>
+          <polyline points="1 9 7 14 15 4"></polyline>
+        </svg>
+      </label>
                   <p className={css.shippinglabel} htmlFor="">My shipping address same as my residential address</p>
                   </div>  
 
